1384d5cebdb66898b84745aff1fcf5988a17a795
Austin Perez <austin.perez760@gmail.com>
2021-02-11 03:12:37 -0800

    created workout collection in mongoDB , created model for workouts using mongoose schema

---
+++ models/workout.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    required: true,
+    deault: () => new Date(),
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Type of excersise is required",
+      },
+
+      name: {
+        type: String,
+        trim: true,
+        required: "Name of excersise is Required",
+      },
+
+      duration: {
+        type: Number,
+        required: "Duration of excersise is required",
+      },
+      distance: Number,
+      weight: Number,
+      reps: Number,
+      setes: Number,
+    },
+  ],
+});
+
+const workout = mongoose.model("workout", workoutSchema);
+module.exports = workout;
